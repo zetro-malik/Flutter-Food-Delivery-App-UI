68c94e8655ce82f7986a735bb696f66542d1f6f0
zetro-malik <zeeshanimsb@gmail.com>
2023-06-29 15:27:00 +0500

    complete cart_screen

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -14,9 +13,9 @@ class CartScreen extends StatefulWidget {
 }
 
 class _CartScreenState extends State<CartScreen> {
-  _buildCartItem(Order item){
-    double price = item.quantity*item.food.price;
-  
+  _buildCartItem(Order item) {
+    double price = item.quantity * item.food.price;
+
     return Container(
       padding: const EdgeInsets.all(20.0),
       height: 170.0,
@@ -27,11 +26,11 @@ class _CartScreenState extends State<CartScreen> {
               Container(
                 width: 150,
                 decoration: BoxDecoration(
-                  image: DecorationImage(image: AssetImage(item.food.imageUrl),fit: BoxFit.cover),
-                borderRadius: BorderRadius.circular(15)
-                ),
+                    image: DecorationImage(
+                        image: AssetImage(item.food.imageUrl),
+                        fit: BoxFit.cover),
+                    borderRadius: BorderRadius.circular(15)),
               ),
-        
               Expanded(
                 child: Container(
                   margin: const EdgeInsets.all(12),
@@ -39,77 +38,161 @@ class _CartScreenState extends State<CartScreen> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(item.food.name, style: const TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold),overflow: TextOverflow.ellipsis,),
+                      Text(
+                        item.food.name,
+                        style: const TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.bold),
+                        overflow: TextOverflow.ellipsis,
+                      ),
                       const SizedBox(height: 10),
-                      Text(item.restaurant.name,style: const TextStyle(fontSize: 16.0,fontWeight: FontWeight.bold,overflow: TextOverflow.ellipsis,)),
-
+                      Text(item.restaurant.name,
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                            overflow: TextOverflow.ellipsis,
+                          )),
                       Container(
                         width: 100,
                         decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10.0),
-                          border:Border.all(
-                            width: 2.0,
-                            color: Colors.black54
-                          )
-                        ),
+                            borderRadius: BorderRadius.circular(10.0),
+                            border:
+                                Border.all(width: 2.0, color: Colors.black54)),
                         child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                        
-                          GestureDetector(onTap: () {
-                            if (item.quantity !=1)
-                            item.quantity =item.quantity-1; 
-                               setState(() {
-                              
-                            });
-                          }, child:const Icon(Icons.remove,color: Colors.deepOrangeAccent,)),
-
-                          Text(item.quantity.toString(),style: const TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
-
-
-                          GestureDetector(onTap: () {
-                          
-                            item.quantity =item.quantity+1; 
-                            setState(() {
-                              
-                            });
-                          }, child:const Icon(Icons.add,color: Colors.deepOrangeAccent,)),
-                        ]),
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              GestureDetector(
+                                  onTap: () {
+                                    if (item.quantity != 1)
+                                      item.quantity = item.quantity - 1;
+                                    setState(() {});
+                                  },
+                                  child: const Icon(
+                                    Icons.remove,
+                                    color: Colors.deepOrangeAccent,
+                                  )),
+                              Text(
+                                item.quantity.toString(),
+                                style: const TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                              GestureDetector(
+                                  onTap: () {
+                                    item.quantity = item.quantity + 1;
+                                    setState(() {});
+                                  },
+                                  child: const Icon(
+                                    Icons.add,
+                                    color: Colors.deepOrangeAccent,
+                                  )),
+                            ]),
                       )
                     ],
                   ),
                 ),
               ),
-              Text('\$${price.toString()}',style: const TextStyle(fontSize: 18,fontWeight: FontWeight.w600),),
+              Text(
+                '\$${price.toString()}',
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+              ),
             ],
           ),
         )
       ]),
     );
   }
-  
-  
+
   @override
   Widget build(BuildContext context) {
+    double totalPrice = 0;
+    currentUser.cart.forEach((element) {
+      totalPrice += element.food.price * element.quantity;
+    });
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.deepOrangeAccent,
+          centerTitle: true,
+          backgroundColor: Colors.deepOrangeAccent,
           title: Text(
-        'Cart (${currentUser.cart.length})',
-        style: TextStyle(fontSize: 18, color: Colors.grey[50]),
-      )),
+            'Cart (${currentUser.cart.length})',
+            style: TextStyle(fontSize: 18, color: Colors.grey[50]),
+          )),
       body: ListView.separated(
-        itemCount: currentUser.cart.length,
+        physics: BouncingScrollPhysics(),
+        itemCount: currentUser.cart.length + 1,
         itemBuilder: (context, index) {
-          Order order = currentUser.cart[index];
-          return _buildCartItem(order);
+          if (index < currentUser.cart.length) {
+            Order order = currentUser.cart[index];
+            return _buildCartItem(order);
+          }
+          return Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      'Estimated Delivery Time',
+                      style: TextStyle(
+                          fontSize: 18.0, fontWeight: FontWeight.w600),
+                    ),
+                    Text(
+                      '25 min',
+                      style: TextStyle(
+                          fontSize: 18.0, fontWeight: FontWeight.w600),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Total Cost',
+                      style: TextStyle(
+                          fontSize: 18.0, fontWeight: FontWeight.w600),
+                    ),
+                    Text(
+                      '\$${totalPrice.toStringAsFixed(2)}',
+                      style: TextStyle(
+                          color: Colors.green[700],
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 100)
+              ],
+            ),
+          );
         },
         separatorBuilder: (context, index) {
-          return Divider(height: 1,color: Colors.grey,);
+          return const Divider(
+            height: 1,
+            color: Colors.grey,
+          );
         },
-      )
+      ),
+      bottomSheet: Container(
+        height: 100.0,
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+            color: Colors.deepOrangeAccent,
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black26, offset: Offset(0, -1), blurRadius: 6.0)
+            ]),
+        child: const Center(
+          child: Text(
+            'CHECKOUT',
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.2),
+          ),
+        ),
+      ),
     );
-
   }
 }
